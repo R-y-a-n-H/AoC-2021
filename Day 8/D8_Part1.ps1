f62232583363b941d746873df99e9a87b954f576
R-y-a-n-H <52160793+R-y-a-n-H@users.noreply.github.com>
2021-12-09 13:07:40 -0500

    Add files via upload

---
+++ Day 8/D8_Part1.ps1
@@ -0,0 +1,18 @@
+﻿$data = Get-Content -Path "C:\Users\hansardr\OneDrive - Verplank Enterprises, Inc\hansardr Profile\Documents\Advent of Code - 2021\Day 8\Input.txt" | ForEach-Object {$_.Split("|")[1].trim(" ")}
+[int]$count = "0"
+
+Foreach ($ReadOut in $Data){
+    $Digits = ($ReadOut).split(" ")
+    ForEach($Digit in $Digits){
+        Switch ($Digit.length)
+        {
+            2 {$count++}
+            4 {$count++}
+            3 {$count++}
+            7 {$count++}
+        }
+
+    }
+}
+
+$count
---
+++ Day 9/D9_Part1.ps1
@@ -0,0 +1,36 @@
+﻿$Height = Get-Content -Path "$PSScriptRoot\D9_Input.txt"
+[int]$Risk = 0
+
+<#
+Sanity Check Notes...
+
+Up =  Y - 1, X
+Down = Y + 1, X
+Left = X - 1, Y
+Right = X + 1, Y
+#>
+
+For($y=0;$y -lt $Height.Count;$y++){ # Get y axis location
+    For($x=0;$x -lt $Height[$y].Length;$x++){ # Get x axis location
+    $loc = $Height[$y][$x] # Get Current Location using y & x
+    # Get postions around current location if possible to then later compare
+    $Up = If($y -gt "0") {$Height[$y - 1][$x]}
+    $Down = If($y -lt $Height.Count) {$Height[$y + 1][$x]}
+    $Left = If($x -gt "0") {$Height[$y][$x - 1]}
+    $Right = If($x -lt $Height.Length) {$Height[$y][$x + 1]}
+
+        If(($Up -ne $null) -or ($Up -lt $Loc)){
+            If(($Down -ne $null) -or ($Down -lt $loc)){
+                If(($Left -ne $null) -or ($left -lt $loc)){
+                    If(($Right -ne $nul) -or ($Right -lt $Loc)){
+                        $Risk += ($loc + 1)
+                    } # Right If
+                }# Left If
+            } # Down If
+        } # Up If
+    } # X for
+} #Y for
+
+$risk # output / Answer
+
+# C:\Users\hansardr\OneDrive - Verplank Enterprises, Inc\hansardr Profile\Documents\Advent of Code - 2021\Day 9
